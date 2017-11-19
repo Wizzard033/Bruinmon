bf68cbcad830c818d28e2d824a9fa56bcf282bb6
Nicholas Turk <nicholasturk@Nicholas-MacBook-Pro.local>
2017-11-18 17:10:14 -0800

    Added initial bluetooth code - to be continued.

--- app/src/main/java/com/bruinmon/PreBattleActivity.java
+++ app/src/main/java/com/bruinmon/PreBattleActivity.java
@@ -4,13 +4,22 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.IntentFilter;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import java.util.Set;
 
 public class PreBattleActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pre_battle);
+
+        // Register for broadcasts when a device is discovered.
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter);
     }
 
     /** Called when the user touches the Battle vs AI button **/
@@ -19,8 +28,52 @@ public class PreBattleActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    // Create a BroadcastReceiver for ACTION_FOUND.
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Discovery has found a device. Get the BluetoothDevice
+                // object and its info from the Intent.
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // Don't forget to unregister the ACTION_FOUND receiver.
+        unregisterReceiver(mReceiver);
+    }
+
     /** Called when the user touches the Battle vs Player button **/
     public void battlePlayer(View view) {
         // TODO : Use Bluetooth to do a player vs. player battle
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) { // Check if device supports bluetooth
+            // Device does not support Bluetooth
+            System.err.println("Bluetooth not supported on device.");
+            System.exit(-1);
+        }
+        int REQUEST_ENABLE_BT = 1;
+        if (!mBluetoothAdapter.isEnabled()) {// Enable bluetooth if not already enabled
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+
+        if (pairedDevices.size() > 0) {
+            // There are paired devices. Get the name and address of each paired device.
+            for (BluetoothDevice device : pairedDevices) {
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+            }
+        }
+
     }
 }
