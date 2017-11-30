edd43abc45b2c28707df8e260069d647d88b88fd
Nicholas Turk <nicholasturk@unk-426d045e.adelphiacom.net>
2017-11-30 01:14:57 -0800

    Bluetooth code works, but need UI to function first

--- app/src/main/java/com/bruinmon/PreBattleActivity.java
+++ app/src/main/java/com/bruinmon/PreBattleActivity.java
@@ -1,8 +1,11 @@
 package com.bruinmon;
 
 import android.content.Intent;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
@@ -14,23 +17,117 @@ import android.widget.Toast;
 import android.view.Gravity;
 import android.bluetooth.BluetoothServerSocket;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.*;
+import android.os.*;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Set;
 import java.util.UUID;
+import java.util.*;
+//import com.example.android.common.activities.SampleActivityBase;
 
-public class PreBattleActivity extends AppCompatActivity {
+public class PreBattleActivity extends SampleActivityBase {
+
+    private OutputStream outputStream;
+    private InputStream inStream;
+    private boolean mLogShown;
+
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> mDeviceList = new ArrayList<String>();
+    private BluetoothAdapter mBluetoothAdapter;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pre_battle);
 
+        //listView = (ListView) findViewById(R.id.listViewer);
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        mDeviceList.add(mBluetoothAdapter.getName()); //hard-coded name of self device
+
+        listView = findViewById(R.id.listViewer);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mDeviceList);
+
+
+/*
+        if (savedInstanceState == null) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            BluetoothMessage fragment = new BluetoothMessage();
+            transaction.replace(R.id.sample_content_fragment, fragment);
+            transaction.commit();
+        }*/
+
+
         // Register for broadcasts when a device is discovered.
-        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        registerReceiver(mReceiver, filter);
+       // IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+       // registerReceiver(mReceiver, filter);
+
+       /* mDeviceList.add(mBluetoothAdapter.getName() + "\n" + mBluetoothAdapter.getAddress());
+        listView.setAdapter(new ArrayAdapter<String>(context,
+                android.R.layout.simple_list_item_1, mDeviceList));*/
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        MenuItem logToggle = menu.findItem(R.id.menu_toggle_log);
+        logToggle.setVisible(findViewById(R.id.sample_output) instanceof ViewAnimator);
+        logToggle.setTitle(mLogShown ? R.string.sample_hide_log : R.string.sample_show_log);
+
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.menu_toggle_log:
+                mLogShown = !mLogShown;
+                ViewAnimator output = findViewById(R.id.sample_output);
+                if (mLogShown) {
+                    output.setDisplayedChild(1);
+                } else {
+                    output.setDisplayedChild(0);
+                }
+                supportInvalidateOptionsMenu();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        // Don't forget to unregister the ACTION_FOUND receiver.
+        //unregisterReceiver(mReceiver);
+        super.onDestroy();
+    }
+
+    // Create a BroadcastReceiver for ACTION_FOUND.
+    /*private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            //if (BluetoothDevice.ACTION_FOUND.equals(action))
+           // {
+                Toast.makeText(getApplicationContext(), "fuck you", Toast.LENGTH_LONG).show();
+                BluetoothDevice device = intent
+                        .getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                mDeviceList.add(device.getName() + " " + device.getAddress());
+                //Log.i("BT", device.getName() + "\n" + device.getAddress());
+                //listView.setAdapter(new ArrayAdapter<String>(context,
+                       // android.R.layout.simple_list_item_1, mDeviceList));
+           // }
+        }
+    };*/
 
     /** Called when the user touches the Battle vs AI button **/
     public void battleAI(View view) {
@@ -38,70 +135,65 @@ public class PreBattleActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    // Create a BroadcastReceiver for ACTION_FOUND.
-    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                // Discovery has found a device. Get the BluetoothDevice
-                // object and its info from the Intent.
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                String deviceName = device.getName();
-                String deviceHardwareAddress = device.getAddress(); // MAC address
-            }
-        }
-    };
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        // Don't forget to unregister the ACTION_FOUND receiver.
-        unregisterReceiver(mReceiver);
-    }
-
     /** Called when the user touches the Battle vs Player button **/
     public void battlePlayer(View view) throws IOException {
         // TODO : Use Bluetooth to do a player vs. player battle
-        UUID myUIID = UUID.randomUUID();
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (mBluetoothAdapter == null) { // Check if device supports bluetooth
-            // Device does not support Bluetooth
-            System.err.println("Bluetooth not supported on device.");
-            System.exit(-1);
-        }
-        int REQUEST_ENABLE_BT = 1;
-        if (!mBluetoothAdapter.isEnabled()) {// Enable bluetooth if not already enabled
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-            //make device discoverable for 300 seconds (5 mins)
-            Intent discoverableIntent =
-                    new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-            discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
-            startActivity(discoverableIntent);
-        }
-        String myDeviceAddress = mBluetoothAdapter.getAddress();
-        String myDeviceName = mBluetoothAdapter.getName();
-        String status = myDeviceName +  " : " + myDeviceAddress;
-
-        Toast.makeText(this, status, Toast.LENGTH_LONG).show();
-
+        Toast.makeText(getApplicationContext(), "hello", Toast.LENGTH_LONG).show();
+        //get paired devices, add to list
         Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        mDeviceList.clear();
+        for(BluetoothDevice bt : pairedDevices)
+            mDeviceList.add(bt.getName());
+        listView.setAdapter(adapter);
 
-        if (pairedDevices.size() > 0) {
-            // There are paired devices. Get the name and address of each paired device.
-            for (BluetoothDevice device : pairedDevices) {
-                String deviceName = device.getName();
-                String deviceHardwareAddress = device.getAddress(); // MAC address
+
+        /*if(pairedDevices.size() > 0) {
+            Object[] devices = (Object []) pairedDevices.toArray();
+            BluetoothDevice device = (BluetoothDevice) devices[position++];
+            ParcelUuid[] uuids = device.getUuids();
+            BluetoothSocket socket = device.createRfcommSocketToServiceRecord(uuids[0].getUuid());
+            socket.connect();
+            outputStream = socket.getOutputStream();
+            inStream = socket.getInputStream();
+        }
+        else {
+            Log.e("error", "No appropriately paired devices.");
+        }*/
+}
+
+
+    public void write(String s) throws IOException
+    {
+        outputStream.write(s.getBytes());
+    }
+
+    public void run()
+    {
+        final int BUFFER_SIZE = 1024;
+        byte[] buffer = new byte[BUFFER_SIZE];
+        int bytes = 0;
+        int b = BUFFER_SIZE;
+
+        while (true)
+        {
+            try
+            {
+                bytes = inStream.read(buffer, bytes, BUFFER_SIZE - bytes);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
             }
         }
-
     }
+
+
 
     public void connectToPlayer(View view) throws IOException { //server setup
         //called when bluetooth connection button is pressed
 
         Toast.makeText(this, "Needs to be implemented with threads!", Toast.LENGTH_LONG).show();
+
         /*UUID myUIID = UUID.randomUUID();
         BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (mBluetoothAdapter == null) { // Check if device supports bluetooth
